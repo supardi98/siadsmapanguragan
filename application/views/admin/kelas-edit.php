b034612cf4ba013a54ed174ed03708a56d001cc2
Supardi Dev <supardi.dev@gmail.com>
2023-05-01 12:25:55 +0700

    css add min-vh-100

--- application/views/admin/kelas-edit.php
+++ application/views/admin/kelas-edit.php
@@ -1,5 +1,5 @@
 <!-- Container Fluid-->
-<div class="container-fluid" id="container-wrapper">
+<div class="container-fluid min-vh-100" id="container-wrapper">
   <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800"><?= $judul; ?></h1>
     <ol class="breadcrumb">
--- application/views/admin/petugas.php
+++ application/views/admin/petugas.php
@@ -5,7 +5,7 @@ $auth = $this->session->userdata('Auth');
 ?>
 
 <!-- Container Fluid-->
-<div class="container-fluid" id="container-wrapper">
+<div class="container-fluid min-vh-100" id="container-wrapper">
   <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800"><?= $judul; ?></h1>
     <ol class="breadcrumb">
